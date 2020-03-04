95ef5e86e3c80f5f73f7229d0dff7c1340bf6eb1
Jharell Chambers <jharell.chambers@selu.edu>
2020-03-04 03:06:06 +0000

    --refresh

--- sinatra_project/db/migrate/02_create_game_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateGamesTable < ActiveRecord::Migration[5.2]
-  def change
-    create_table :games do |t|
-      t.string :name
-      t.integer :owner_id
-    end
-  end
-end
--- sinatra_project/db/seeds.rb
+++
@@ -1,4 +0,0 @@
-# Add seed data here. Seed your database with `rake db:seed`
-sophie = Owner.create(name: "Sophie")
-Game.create(name: "Gears of War", owner: sophie)
-Game.create(name: "Need For Speed", owner: sophie)
