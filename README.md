e1593ea87f7037746acf02ab3d00ab1940e60b52
Jharell Chambers <jharell.chambers@selu.edu>
2020-03-08 23:14:42 +0000

    --ff2

---
+++ README.md
@@ -0,0 +1,6 @@
+Welcome to MyGames!
+MyGames is an easy-to-use web-based collection management system.
+
+Instructions:
+
+-
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ActiveRecord::Base
   has_many  :games
-
-
   has_secure_password
 
   def slug
@@ -11,5 +9,4 @@ class User < ActiveRecord::Base
   def self.find_by_slug(slug)
     User.all.find{|user| user.slug == slug}
   end
-
 end
