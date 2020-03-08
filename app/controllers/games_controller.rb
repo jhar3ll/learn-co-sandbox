34914ab7624c98d3d5edd75d280c6fbf42965261
Jharell Chambers <jharell.chambers@selu.edu>
2020-03-08 21:04:10 +0000

    --polishing

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,5 @@
 class GamesController < ApplicationController
-  
+
   get '/games' do
     if logged_in?
       @games = Game.all
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,16 +40,16 @@ class UsersController < ApplicationController
       session[:user_id] = user.id
       redirect to "/games"
     else
-      redirect to '/signup'
+      redirect to '/login'
     end
   end
 
   get '/logout' do
     if logged_in?
       session.clear
-      redirect to '/login' 
+      redirect to '/' 
     else 
-      redirect to '/'
+      redirect to '/login'
     end 
   end
 end
